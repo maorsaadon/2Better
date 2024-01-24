871a63c2a3de1abaf7800ab394eb806622856251
AviyaArusi <aviyaarusi@gmail.com>
2024-01-24 19:23:46 +0200

    Pull User data

--- back/UserService.js
+++ back/UserService.js
@@ -1,48 +1,36 @@
-// src/services/UserService.js
 import { auth, db } from '../back/firebase'
-
 
 var userFirstName = ""
 var userLastName = ""
 var UserCity = ""
-// const userEmail = auth.currentUser.email;
-// const userEmail = "aviya1@test.com";
+var userMyGroups = []  // Added variable to store user's groups
 
 export const UserService = {
   // Fetch user details by UID
   async getUserDetails() {
     try {
       const userEmail = auth.currentUser.email;
-      const snapshot = db.collection('Users').doc(userEmail);
-            snapshot.get()
-            .then( docSnapshot => {
-              if (docSnapshot.exists) {
-                const userData = docSnapshot.data(); 
-                userFirstName = userData.FirstName;
-                userLastName = userData.LastName;
-                UserCity = userData.City;
-              } else {
-                // Handle the case where the document does not exist
-                console.log('No such document!');
-              }
-            })
-            //     )(doc => ({
-            //     city: doc.data().city,
-            //     Email: doc.data().Email,
-            //     FirstName: doc.data().FirstName,
-            //     LastName: doc.data().LastName,
-            //     MyGroups: doc.data().MyGroups
-            // }));            
-            // return groups;
-        } catch (error) {
-            console.error("Error fetching User: ", error);
-            throw error;
-        }
-  },
+      const snapshot = await db.collection('Users').doc(userEmail).get();
 
+      if (snapshot.exists) {
+        const userData = snapshot.data(); 
+        userFirstName = userData.FirstName;
+        userLastName = userData.LastName;
+        UserCity = userData.City;
+        userMyGroups = userData.MyGroups || []; // Assign MyGroups if it exists, else default to an empty array
+
+        // Log the user's groups to the console
+        console.log('User Groups:', userMyGroups[0]);
+      } else {
+        // Handle the case where the document does not exist
+        console.log('No such document!');
+      }
+    } catch (error) {
+      console.error("Error fetching User: ", error);
+    }
+  },
 };
 
-
-export { userFirstName, userLastName, UserCity};
+export { userFirstName, userLastName, UserCity, userMyGroups };
 
 export default UserService;
