d25962d08702d246aabe792806f3727f032e7f84
AviyaArusi <aviyaarusi@gmail.com>
2024-02-07 15:11:16 +0200

    New UpcomingMeeting Style

--- components/MeetingCard.js
+++ components/MeetingCard.js
@@ -1,27 +1,25 @@
 import {
-  Pressable,
-  Text,
-  View,
-  SafeAreaView,
-  StyleSheet,
-  Dimensions,
-  TouchableOpacity,
+Pressable,
+Text,
+View,
+SafeAreaView,
+StyleSheet,
+Dimensions,
+TouchableOpacity,
 } from "react-native";
 import {
-  AntDesign,
-  FontAwesome5,
-  MaterialIcons,
-  FontAwesome,
-  MaterialCommunityIcons,
+AntDesign,
+FontAwesome5,
+MaterialIcons,
+FontAwesome,
+MaterialCommunityIcons,
 } from "@expo/vector-icons";
 import CustomSlider from "./CustomSlider";
 import {
-  sportIconMapping_MaterialCommunityIcons,
-  sportIconMapping_FontAwesome,
-  sportIconMapping_FontAwesome5,
+sportIconMapping_MaterialCommunityIcons,
+sportIconMapping_FontAwesome,
+sportIconMapping_FontAwesome5,
 } from "../back/DataBase";
-import { useNavigation } from "@react-navigation/core";
-import NotificationService from "../back/NotificationsService";
 import { auth } from "../back/firebase";
 import React, { useEffect, useState } from "react";
 import { userFirstName, userLastName, UserCity } from "../back/UserService";
@@ -29,113 +27,99 @@ import { serverTimestamp } from "firebase/firestore";
 
 const screenWidth = Dimensions.get("window").width;
 
-const ResultGroupCard = ({ group }) => {
+const MeetingCard = ({ meeting }) => {
 
-  const navigation = useNavigation();
-  const groupName = group?.GroupName ?? "Default Name";
-  const currentParticipants = parseInt(group.Members, 10);
-  const totalCapacity = parseInt(group.TotalCapacity, 10);
-  // const userEmail = auth.currentUser.email;
-  // const [isEditing, setIsEditing] = useState(false);
-  // const [firstName, setFirstName] = useState("");
-  // const [lastName, setLastName] = useState("");
-  const content = "`" + userFirstName + " " + userLastName + "` wants to join `" + groupName +"`"
+const groupName = meeting?.GroupName ?? "Default Name";
+// const currentParticipants = parseInt(meeting.Members, 10);
+// const totalCapacity = parseInt(meeting.TotalCapacity, 10);
+const currentParticipants = parseInt(3);
+const totalCapacity = parseInt(10);
 
-  // useEffect(() => {
-  //   const fetchUserDetails = async () => {
-  //     try {
-  //       setFirstName(userFirstName);
-  //       setLastName(userLastName);
-  //     } catch (error) {
-  //       console.error("Error fetching user details:", error);
-  //     }
-  //   };
+const content = "`" + userFirstName + " " + userLastName + "` wants to join `" + groupName +"`"
 
-  //   fetchUserDetails();
-  // }, []);
-
-  const getSportIcon = (sportType) => {
+const getSportIcon = (sportType) => {
     const iconName = sportIconMapping_FontAwesome5[sportType];
     if (iconName) {
-      return <FontAwesome5 name={iconName} size={30} color="black" />;
+    return <FontAwesome5 name={iconName} size={30} color="black" />;
     }
     const iconNameFA = sportIconMapping_FontAwesome[sportType];
     if (iconNameFA) {
-      return <FontAwesome name={iconNameFA} size={30} color="black" />;
+    return <FontAwesome name={iconNameFA} size={30} color="black" />;
     }
     const iconNameMCI = sportIconMapping_MaterialCommunityIcons[sportType];
     if (iconNameMCI) {
-      return (
+    return (
         <MaterialCommunityIcons name={iconNameMCI} size={30} color="black" />
-      );
+    );
     }
 
     return null; // Return null if no icon is found
-  };
+};
 
-  const handleJoinPress = () =>{
-    NotificationService.handleAddNewNotification(group.LeaderEmail, content, "Group joining request", serverTimestamp())
-  };
+const handleJoinPress = () =>{
+    // NotificationService.handleAddNewNotification(group.LeaderEmail, content, "Group joining request", serverTimestamp())
+    console.log("Click on hendel Join Meeting!");
+};
 
-  return (
+return (
     <SafeAreaView>
-      <View style={styles.card}>
+    <View style={styles.card}>
         <View style={styles.cardTopRow}>
-          {getSportIcon(group.SportType)}
-          <View>
+        {getSportIcon(meeting.SportType)}
+        <View>
             <Text style={styles.title}>{groupName}</Text>
-            <Text style={styles.subTitle}>{group.SportType}</Text>
-          </View>
+            <Text style={styles.subTitle}>{meeting.SportType}</Text>
+        </View>
         </View>
         <View style={styles.cardMiddleRow}>
-          <View style={styles.iconAndTextContainer}>
+        <View style={styles.iconAndTextContainer}>
             <MaterialIcons name="location-on" size={22} color="black" />
-            <Text>{group.City}</Text>
-          </View>
-          <View style={styles.iconAndTextContainer}>
-            <MaterialCommunityIcons name="email" size={22} color="black" />
-            <Text>{group.LeaderEmail}</Text>
-          </View>
+            <Text>{meeting.Location}</Text>
+        </View>
+        <View style={styles.iconAndTextContainer}>
+            <MaterialCommunityIcons name="calendar-clock" size={22} color="black" />
+            <Text>{/*meeting.Time*/} , {meeting.Date}</Text>
+        </View>
         </View>
         <View style={styles.participantContainer}>
-          <Text style={styles.participantText}>{currentParticipants}</Text>
-          <CustomSlider
+        <Text style={styles.participantText}>{currentParticipants}</Text>
+        <CustomSlider
             minimumValue={0}
             maximumValue={totalCapacity}
             value={currentParticipants}
-          />
-          <Text style={styles.participantText}>{totalCapacity}</Text>
-          <AntDesign name="user" size={22} color="black" />
+        />
+        <Text style={styles.participantText}>{totalCapacity}</Text>
+        <AntDesign name="user" size={22} color="black" />
         </View>
         <View style={styles.cardBottomRow}>
-          <TouchableOpacity 
-          style={styles.button}
-          onPress={handleJoinPress}
-          >
-            <Text style={styles.buttonText}>Subscribe</Text>
-          </TouchableOpacity>
+        <TouchableOpacity 
+        style={styles.button}
+        onPress={handleJoinPress}
+        >
+            <Text style={styles.buttonText}>Join Meeting</Text>
+        </TouchableOpacity>
         </View>
-      </View>
+    </View>
     </SafeAreaView>
-  );
+);
 };
 
-export default ResultGroupCard;
+export default MeetingCard;
 
 const styles = StyleSheet.create({
-  cardBottomRow: {
+cardBottomRow: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-around",
-  },
-  container: {
+},
+container: {
     backgroundColor: "#5B8BDF",
     alignItems: "center",
     paddingBottom: 40,
     paddingTop: 30,
     gap: 15,
-  },
-  card: {
+},
+card: {
     width: screenWidth - 32,
     marginTop: -30,
     backgroundColor: "#FFFFFF", // Assuming a white card background
@@ -150,74 +134,74 @@ const styles = StyleSheet.create({
     elevation: 3, // Elevation for Android
     borderWidth: 1, // Border width
     borderColor: "#E0E0E0",
-  },
-  cardTopRow: {
+},
+cardTopRow: {
     marginTop: 0, // Adjust as needed to move closer to the top
     marginLeft: 0, // Adjust as needed for left alignment
     alignSelf: "flex-start", // Align self to the start of the cross axis
     flexDirection: "row",
     gap: 15,
     alignItems: "center",
-  },
-  cardMiddleRow: {
+},
+cardMiddleRow: {
     flexDirection: "row",
     gap: 15,
     alignItems: "center",
-  },
-  iconAndTextContainer: {
+},
+iconAndTextContainer: {
     flexDirection: "row",
     alignItems: "center",
     gap: 5,
     marginLeft: 0,
-  },
-  sportIcon: {
+},
+sportIcon: {
     width: 30, // Adjust size as needed
     height: 30, // Adjust size as needed
     resizeMode: "contain",
     marginRight: 10, // Add some space between the icon and the text
-  },
-  title: {
+},
+title: {
     fontWeight: "bold",
     fontSize: 18,
     alignSelf: "flex-start",
     marginLeft: 0,
-  },
-  subTitle: {
+},
+subTitle: {
     opacity: 0.6,
     alignSelf: "flex-start",
     marginLeft: 0,
     fontSize: 16,
     color: "gray",
-  },
-  button: {
+},
+button: {
     backgroundColor: "#3B82F6",
     width: 120,
     paddingVertical: 10,
     borderRadius: 10,
-  },
-  buttonText: {
+},
+buttonText: {
     alignSelf: "center",
     color: "white",
-  },
-  participantContainer: {
+},
+participantContainer: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
     paddingVertical: 10,
-  },
-  slider: {
+},
+slider: {
     flex: 1,
     height: 40,
     marginHorizontal: 10,
     minimumTrackTintColor: "black",
     maximumTrackTintColor: "#C0C0C0", // Color for the remaining track
     thumbTintColor: "white",
-  },
-  participantText: {
+},
+participantText: {
     fontSize: 16,
     color: "#000",
     fontWeight: "bold",
     // Add margin to the left or right to space the text from the slider
     marginHorizontal: 5,
-  },
+},
 });
