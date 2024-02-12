874b504fe86551ce2614544e5ce95fbd9657722e
Ashwal200 <Ashwal200@gmail.com>
2024-02-12 13:49:42 +0200

    navigate bar

--- App.js
+++ App.js
@@ -1,7 +1,56 @@
+// import React from "react";
+// import { StyleSheet } from "react-native";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import AppScreen from "./front/AppScreen";
+// import LoginScreen from "./front/LoginScreen";
+// import HomeScreen from "./front/HomeScreen";
+// import RegisterScreen from "./front/RegisterScreen";
+// import ProfileScreen from "./front/ProfileScreen";
+// import EditProfileScreen from "./front/EditProfileScreen";
+// import MyGroupScreen from "./front/MyGroupScreen";
+// import FindNewGroupsScreen from "./front/FindNewGroupsScreen";
+// import NotificationsScreen from "./front/NotificationsScreen";
+// import UpcomingMeetingScreen from "./front/UpcomingMeetingScreen";
+// import AddNewMeetingScreen from "./front/AddNewMeetingScreen";
+// import AddNewGroupScreen from "./front/AddNewGroupScreen";
+// import ResultGroupsScreen from "./front/ResultGroupsScreen";
+// import EditGroupScreen from "./front/EditGroupScreen";
+// import EditMeetingScreen from "./front/EditMeetingScreen";
+
+// const Stack = createNativeStackNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="Entry" component={AppScreen} />
+//         <Stack.Screen name="Login" component={LoginScreen} />
+//         <Stack.Screen name="Register" component={RegisterScreen} />
+//         <Stack.Screen name="Home" component={HomeScreen} />
+//         <Stack.Screen name="Profile" component={ProfileScreen} />
+//         <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+//         <Stack.Screen name="MyGroups" component={MyGroupScreen} />
+//         <Stack.Screen name="FindNewGroups" component={FindNewGroupsScreen} />
+//         <Stack.Screen name="Notifications" component={NotificationsScreen} />
+//         <Stack.Screen
+//           name="UpComingMeetings"
+//           component={UpcomingMeetingScreen}
+//         />
+//         <Stack.Screen name="AddNewGroup" component={AddNewGroupScreen} />
+//         <Stack.Screen name="AddNewMeeting" component={AddNewMeetingScreen} />
+//         <Stack.Screen name="ResultGroups" component={ResultGroupsScreen} />
+//         <Stack.Screen name="EditGroup" component={EditGroupScreen} />
+//         <Stack.Screen name="EditMeetingScreen" component={EditMeetingScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
 import React from "react";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import AppScreen from "./front/AppScreen";
 import LoginScreen from "./front/LoginScreen";
 import HomeScreen from "./front/HomeScreen";
@@ -17,8 +66,66 @@ import AddNewGroupScreen from "./front/AddNewGroupScreen";
 import ResultGroupsScreen from "./front/ResultGroupsScreen";
 import EditGroupScreen from "./front/EditGroupScreen";
 import EditMeetingScreen from "./front/EditMeetingScreen";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const GroupsStack = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="MyGroup" component={MyGroupScreen} />
+      <Stack.Screen name="AddNew" component={AddNewMeetingScreen} />
+      <Stack.Screen name="EditGroup" component={EditGroupScreen} />
+      <Stack.Screen name="AddNewGroup" component={AddNewGroupScreen} />
+    </Stack.Navigator>
+  );
+};
+
+const MeetingStack = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Meeting" component={UpcomingMeetingScreen} />
+      <Stack.Screen name="EditMeeting" component={EditMeetingScreen} />
+    </Stack.Navigator>
+  );
+};
+
+const HomeStack = () => {
+  return (
+    <Tab.Navigator screenOptions= {({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        
+        let iconName;
+
+        if (route.name === 'Home Page') {
+          iconName = focused ? 'home' : 'home-outline';
+        } else if (route.name === 'My Groups') {
+          iconName = focused ? 'people' : 'people-outline';
+        } else if (route.name === 'Explore Group') {
+          iconName = focused ? 'compass' : 'compass-outline';
+        } else if (route.name === 'Meetings') {
+          iconName = focused ? 'calendar' : 'calendar-outline';
+        } else if (route.name === 'Log Out') {
+          iconName = focused ? 'exit' : 'exit-outline';
+        }
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+    })}
+      tabBarOptions={{
+        activeTintColor: '#366A68',
+        inactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen name="Home Page" component={HomeScreen} />
+      <Tab.Screen name="My Groups" component={GroupsStack} />
+      <Tab.Screen name="Explore Group" component={FindNewGroupsScreen} />    
+      <Tab.Screen name="Meetings" component={MeetingStack} />
+      <Tab.Screen name="Log Out" component={NotificationsScreen} />
+
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   return (
@@ -27,21 +134,10 @@ export default function App() {
         <Stack.Screen name="Entry" component={AppScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeStack} options={{ headerShown: false }}/>
         <Stack.Screen name="Profile" component={ProfileScreen} />
         <Stack.Screen name="EditProfile" component={EditProfileScreen} />
-        <Stack.Screen name="MyGroups" component={MyGroupScreen} />
-        <Stack.Screen name="FindNewGroups" component={FindNewGroupsScreen} />
         <Stack.Screen name="Notifications" component={NotificationsScreen} />
-        <Stack.Screen
-          name="UpComingMeetings"
-          component={UpcomingMeetingScreen}
-        />
-        <Stack.Screen name="AddNewGroup" component={AddNewGroupScreen} />
-        <Stack.Screen name="AddNewMeeting" component={AddNewMeetingScreen} />
-        <Stack.Screen name="ResultGroups" component={ResultGroupsScreen} />
-        <Stack.Screen name="EditGroup" component={EditGroupScreen} />
-        <Stack.Screen name="EditMeetingScreen" component={EditMeetingScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- front/HomeScreen.js
+++ front/HomeScreen.js
@@ -12,31 +12,29 @@ import { auth } from "../back/firebase";
 import myLogoPic from "../assets/default.png";
 import { userFirstName, userLastName, UserCity } from "../back/UserService";
 import UserService from "../back/UserService";
-import { doc, onSnapshot } from 'firebase/firestore';
 // import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 // import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import { db } from "../back/firebase";
+import { styles } from "./ResultGroupsScreen";
 //import {stylesHome} from "../components/StylesSheets";
 
 const HomeScreen = () => {
 
   const navigation = useNavigation();
-  const [userNotificationCounter, setNotificationCounter] = useState(0);
-  console.log(`NotificationCounter = ${userNotificationCounter}`);
   //const Tab = createBottomTabNavigator();
-
   useEffect(() => {
     const fetchData = async () => {
       await UserService.getUserDetails();
     };
-
-    const userRef = doc(db, 'Users', auth.currentUser.email);
-
-    const unsubscribe = onSnapshot(userRef, (doc) => {
-      const data = doc.data();
-      setNotificationCounter(data.NotificationCounter)
-    });
+    // const fetchData = async () => {
+    //   try {
+    //     const data = await UserService.getUserDetails();
+    //     // Handle the data
+    //   } catch (error) {
+    //     console.error('Error fetching data:', error);
+    //   }
+    // };
 
     fetchData();
     return () => unsubscribe();
@@ -93,46 +91,19 @@ const HomeScreen = () => {
 
   return (
     <ImageBackground source={myLogoPic} style={stylesHome.backgroundImage}>
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+      <View style={stylesHome.container}>
 
         <TouchableOpacity onPress={handleNotifications} style={stylesHome.notificationBotton}>
           <Entypo name="bell" size={30} color="black" />
-
-          {/* Badge view */}
-          {userNotificationCounter > 0 && (
-            <View style={stylesHome.badge}>
-              <Text style={stylesHome.badgeText}>{userNotificationCounter}</Text>
-            </View>
-          )}
         </TouchableOpacity>
 
-
-      </View>
-      <TouchableOpacity onPress={handleMyProfile} style={stylesHome.profileButton}>
-        <FontAwesome name="user" size={30} color="black" />
-      </TouchableOpacity>
-      <View style={stylesHome.container}>
-
-
-        <TouchableOpacity onPress={handleMyGroups} style={stylesHome.button}>
-          <FontAwesome6 name="people-group" size={30} color="black" />
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={handleFindNewGroup} style={stylesHome.button}>
-          <MaterialIcons name="explore" size={30} color="black" />
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          onPress={handleUpcomingMeetings}
-          style={stylesHome.button}
-        >
-          <Entypo name="cake" size={30} color="black" />
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={handleSignOut} style={stylesHome.button}>
-          <Entypo name="log-out" size={30} color="black" />
+        <TouchableOpacity onPress={handleMyProfile} style={stylesHome.profileButton}>
+          <FontAwesome name="user" size={30} color="black" />
         </TouchableOpacity>
       </View>
+
+
+
 
     </ImageBackground>
   );
@@ -142,45 +113,15 @@ export default HomeScreen;
 
 const stylesHome = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    flexDirection: 'row', // Arrange buttons horizontally
-    paddingHorizontal: 10, // Add some horizontal padding
-    paddingTop: 520, // Add some horizontal padding
-  },
-  button: {
-    width: "20%",
-    padding: 15,
-    borderRadius: 10,
-    alignItems: "center",
-    marginTop: 40,
-  },
-  buttonText: {
-    color: "white",
-    fontWeight: "700",
-    fontSize: 16,
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    marginBottom: 600
   },
   backgroundImage: {
     flex: 1,
     width: "100%",
     height: "100%",
     justifyContent: "center",
-  },
-  badge: {
-    position: 'absolute',
-    top: 20, // Move it down a bit
-    right: -10, // Move it to the left a bit
-    backgroundColor: 'red',
-    borderRadius: 15,
-    width: 30,
-    height: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  badgeText: {
-    color: 'white', // Number color
-    fontWeight: 'bold',
   },
   notificationBotton: {
     padding: 10,
@@ -192,25 +133,20 @@ const stylesHome = StyleSheet.create({
   profileButton: {
     padding: 10,
     borderRadius: 10,
-    marginTop: -50,
-    marginLeft: 345,
+    marginTop: 0,
+    marginLeft: 280,
     borderRadius: 50,
   }
 })
 
-//#################################################################
 
-// import { StyleSheet, Text, View } from 'react-native'
-// import React from 'react'
 
-// const Homescreen = () => {
-//   return (
-//     <View>
-//       <Text>Homescreen</Text>
-//     </View>
-//   )
-// }
 
-// export default Homescreen
-
-// const styles = StyleSheet.create({})
+    // const fetchData = async () => {
+    //   try {
+    //     const data = await UserService.getUserDetails();
+    //     // Handle the data
+    //   } catch (error) {
+    //     console.error('Error fetching data:', error);
+    //   }
+    // };
