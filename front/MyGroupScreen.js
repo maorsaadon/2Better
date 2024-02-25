e65cd82da894cb6b3db4ef72d5d27f8f2e73cf8c
Ashwal200 <Ashwal200@gmail.com>
2024-02-25 09:27:49 +0200

    refresh groups

--- front/MyGroupScreen.js
+++ front/MyGroupScreen.js
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   ImageBackground,
   ScrollView,
+  RefreshControl
 } from "react-native";
 import { useNavigation } from "@react-navigation/core";
 import myLogoPic from "../assets/default.png";
@@ -19,30 +20,32 @@ const MyGroupsScreen = () => {
   const [isManagerView, setIsManagerView] = useState(true);
   const [ManagerGroups, setManagerGroups] = useState([]);
   const [MemberGroups, setMemberGroups] = useState([]);
+  const [refresh, setRefresh] = useState(false);
+  const [isRefreshing, setRefreshing] = useState(true);
 
-  useEffect(() => {
-    const fetchGroups = async () => {
-      try {
-        if (isManagerView) {
-          const fetchManagerGroups = await GroupService.getManagerGroups();
-          if (Array.isArray(fetchManagerGroups)) {
-            setManagerGroups(fetchManagerGroups);
-          } else {
-            setManagerGroups([]);
-          }
+
+  const fetchGroups = async () => {
+    try {
+      if (isManagerView) {
+        const fetchManagerGroups = await GroupService.getManagerGroups();
+        if (Array.isArray(fetchManagerGroups)) {
+          setManagerGroups(fetchManagerGroups);
         } else {
-          const fetchMemberGroups = await GroupService.getMemberGroups();
-          if (Array.isArray(fetchMemberGroups)) {
-            setMemberGroups(fetchMemberGroups);
-          } else {
-            setMemberGroups([]);
-          }
+          setManagerGroups([]);
         }
-      } catch (error) {
-        console.error("Error fetching groups:", error);
+      } else {
+        const fetchMemberGroups = await GroupService.getMemberGroups();
+        if (Array.isArray(fetchMemberGroups)) {
+          setMemberGroups(fetchMemberGroups);
+        } else {
+          setMemberGroups([]);
+        }
       }
-    };
-
+    } catch (error) {
+      console.error("Error fetching groups:", error);
+    }
+  };
+  useEffect(() => {
     fetchGroups();
   }, [isManagerView]);
 
@@ -58,10 +61,20 @@ const MyGroupsScreen = () => {
   const navigateToGroupMeetings = (group) => {
     console.log("go to the group:", group);
     try {
-      navigation.replace("GroupMeetings", {group});
+      navigation.replace("GroupMeetings", { group });
     } catch (error) {
       alert(error.message);
     }
+  };
+  const onRefreshing = () => {
+    setRefresh(true);
+    setRefreshing(false);
+    fetchGroups();
+    setTimeout(() => {
+      setRefresh(false);
+      setRefreshing(true);
+    }, 2000)
+
   };
 
   return (
@@ -88,17 +101,26 @@ const MyGroupsScreen = () => {
           <Text style={styles.buttonText}>A MEMBER</Text>
         </TouchableOpacity>
       </View>
-      <ScrollView>
+      <ScrollView refreshControl={ // Notice the correct prop name here: refreshControl instead of RefreshControl
+        <RefreshControl
+          refreshing={refresh}
+          onRefresh={() => onRefreshing()}
+          colors={['#366A68', 'black']} // Set the colors of the loading indicator
+          progressBackgroundColor='#E9EFE8'
+          size="large"
+        />
+      }
+      >
         <View style={styles.container}>
           {isManagerView
             ? ManagerGroups.map((group, index) => (
               <TouchableOpacity key={index} onPress={() => navigateToGroupMeetings(group)}>
-              <ManagerGroupCard key={index} group={group} />
+                <ManagerGroupCard key={index} group={group} />
               </TouchableOpacity>
             ))
-            : MemberGroups.map((group, index) => (
+            : isRefreshing ? MemberGroups.map((group, index) => (
               <MemberGroupCard key={index} group={group} />
-            ))}
+            )) : <Text></Text>}
         </View>
       </ScrollView>
       {isManagerView && (
