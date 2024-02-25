595d97fb1f3bf2b38ddbb3ea5838e903c02dc5d0
Ashwal200 <Ashwal200@gmail.com>
2024-02-25 21:56:48 +0200

    image to user in chat

--- front/ChatScreen.js
+++ front/ChatScreen.js
@@ -1,123 +1,147 @@
 import React, {
-    useState,
-    useEffect,
-    useLayoutEffect,
-    useCallback
-  } from 'react';
-  import { TouchableOpacity, Text, View, StyleSheet } from 'react-native';
-  import { GiftedChat } from 'react-native-gifted-chat';
-  import {
-    collection,
-    addDoc,
-    orderBy,
-    query,
-    onSnapshot
-  } from 'firebase/firestore';
-  import { signOut } from 'firebase/auth';
-  import { auth, db } from '../back/firebase';
-  import { useNavigation } from '@react-navigation/native';
-  import { AntDesign } from '@expo/vector-icons';
-  import colors from '../colors';
+  useState,
+  useEffect,
+  useLayoutEffect,
+  useCallback
+} from 'react';
+import { TouchableOpacity, Text, View, StyleSheet, ImageBackground } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import {
+  collection,
+  addDoc,
+  orderBy,
+  query,
+  onSnapshot
+} from 'firebase/firestore';
+import { signOut } from 'firebase/auth';
+import { auth, db } from '../back/firebase';
+import { useNavigation } from '@react-navigation/native';
+import { AntDesign } from '@expo/vector-icons';
+import colors from '../colors';
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+import { userImageUpload } from "../back/UserService";
+import myLogoPic from "../assets/default.png";
+
+const ChatScreen = ({ route }) => {
+  const { meeting } = route.params;
+  const [messages, setMessages] = useState([]);
+  const navigation = useNavigation();
+  const [imageUrl, setImageUrl] = useState(null);
+  console.log(meeting);
 
 
-   const ChatScreen = ({route}) => {
-    const {meeting} = route.params;
-    const [messages, setMessages] = useState([]);
-    const navigation = useNavigation();
+  const getImageURL = async () => {
+    const storage = getStorage();
+    var storageRef;
 
-    console.log(meeting);
+    if (userImageUpload == 0) {
+      storageRef = ref(storage, 'UsersProfilePics/' + 'defaultProfile.jpeg');
+    }
+    else {
+      storageRef = ref(storage, 'UsersProfilePics/' + auth.currentUser.email);
+    }
+    try {
+      const url = await getDownloadURL(storageRef);
+      setImageUrl(url);
+      console.log(url)
+    } catch (error) {
+      console.error('Error retrieving image:', error);
+    }
+  }
 
-    const backButton = () => {
-      try {
-        navigation.replace("Meeting");
-      } catch (error) {
-        alert(error.message);
-      }
-    };
+  const backButton = () => {
+    try {
+      navigation.replace("Meeting");
+    } catch (error) {
+      alert(error.message);
+    }
+  };
 
-    const onSignOut = () => {
-      signOut(auth).catch(error => console.log('Error logging out: ', error));
-    };
+  const onSignOut = () => {
+    signOut(auth).catch(error => console.log('Error logging out: ', error));
+  };
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-          headerRight: () => (
-            <TouchableOpacity
-              style={{
-                marginRight: 10
-              }}
-              onPress={onSignOut}
-            >
-              <AntDesign name="logout" size={24} color={colors.gray} style={{marginRight: 10}}/>
-            </TouchableOpacity>
-          )
-        });
-      }, [navigation]);
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          style={{
+            marginRight: 10
+          }}
+          onPress={onSignOut}
+        >
+          <AntDesign name="logout" size={24} color={colors.gray} style={{ marginRight: 10 }} />
+        </TouchableOpacity>
+      )
+    });
+  }, [navigation]);
 
-    useLayoutEffect(() => {
+  useLayoutEffect(() => {
 
-      const chatRef = collection(db, 'Meetings', meeting.id, 'chat');
-      const q = query(chatRef, orderBy('createdAt', 'desc'));
+    const chatRef = collection(db, 'Meetings', meeting.id, 'chat');
+    const q = query(chatRef, orderBy('createdAt', 'desc'));
 
     const unsubscribe = onSnapshot(q, querySnapshot => {
-        console.log('querySnapshot unsusbscribe');
-          setMessages(
-            querySnapshot.docs.map(doc => ({
-              _id: doc.data()._id,
-              createdAt: doc.data().createdAt.toDate(),
-              text: doc.data().text,
-              user: doc.data().user
-            }))
-          );
-        });
-    return unsubscribe;
-      }, []);
-
-    const onSend = useCallback((messages = []) => {
-        setMessages(previousMessages =>
-          GiftedChat.append(previousMessages, messages)
-        );
-        // setMessages([...messages, ...messages]);
-        const { _id, createdAt, text, user } = messages[0];    
-        const chatRef = collection(db, 'Meetings', meeting.id, 'chat');
-        addDoc(chatRef, {
-          _id,
-          createdAt,
-          text,
-          user
-        });
-      }, []);
-
-      return (
-        <View style={styles.container}>
-          {/* Back button */}
-          <View style={styles.backContainer}>
-            <TouchableOpacity onPress={backButton} style={styles.backButton}>
-              <AntDesign name="back" size={24} color="#366A68" />
-            </TouchableOpacity>
-          </View>
-    
-          {/* GiftedChat */}
-          <GiftedChat
-            messages={messages}
-            showAvatarForEveryMessage={false}
-            showUserAvatar={false}
-            onSend={messages => onSend(messages)}
-            messagesContainerStyle={{
-              backgroundColor: 'white', // Make it transparent
-            }}
-            textInputStyle={{
-              backgroundColor: '#fff',
-              borderRadius: 20,
-            }}
-            user={{
-              _id: auth?.currentUser?.email,
-              avatar: 'https://i.pravatar.cc/300'
-            }}
-          />
-        </View>
+      console.log('querySnapshot unsusbscribe');
+      setMessages(
+        querySnapshot.docs.map(doc => ({
+          _id: doc.data()._id,
+          createdAt: doc.data().createdAt.toDate(),
+          text: doc.data().text,
+          user: doc.data().user
+        }))
       );
-    };
+    });
+    return unsubscribe;
+  }, []);
+
+  const onSend = useCallback((messages = []) => {
+    getImageURL();
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages)
+    );
+    // setMessages([...messages, ...messages]);
+    const { _id, createdAt, text, user } = messages[0];
+    const chatRef = collection(db, 'Meetings', meeting.id, 'chat');
+    addDoc(chatRef, {
+      _id,
+      createdAt,
+      text,
+      user
+    });
+  }, []);
+
+  return (
+    <ImageBackground source={myLogoPic} style={styles.backgroundImage}>
+      <View style={styles.container}>
+        <View style={styles.backContainer}>
+          <TouchableOpacity onPress={backButton} style={styles.backButton}>
+            <AntDesign name="back" size={24} color="#366A68" />
+          </TouchableOpacity>
+        </View>
+
+        {/* GiftedChat */}
+        <GiftedChat
+          messages={messages}
+          showAvatarForEveryMessage={false}
+          showUserAvatar={true}
+          onSend={messages => onSend(messages)}
+          messagesContainerStyle={{
+            backgroundColor: 'white', // Make it transparent
+          }}
+          textInputStyle={{
+            backgroundColor: '#fff',
+            borderRadius: 20,
+          }}
+          user={{
+            _id: auth?.currentUser?.email,
+            avatar: imageUrl
+          }}
+        />
+      </View>
+    </ImageBackground>
+  );
+};
 
 export default ChatScreen;
 
@@ -134,5 +158,11 @@ const styles = StyleSheet.create({
   },
   backButton: {
     padding: 10,
+  },
+  backgroundImage: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
   },
 });
